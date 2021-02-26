9f872881d30132d2295d647064f824bdcbde2288
OmerPasa3328 <omerfa33@gmail.com>
2021-02-27 02:22:19 +0300

    new things :D

---
+++ using System.Collections.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Kata
+{
+  public static string SpinWords(string sentence)
+  {
+    string[]
+    
+    
+    
+    
+    
+    return (sentence);
+  }
+}
