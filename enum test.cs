4b19f98ab9f38489e14912c378556881d6cccdaf
OmerPasa3328 <omerfa33@gmail.com>
2021-03-15 22:31:56 +0300

    a bit more

--- enum test.cs
+++ enum test.cs
@@ -1,15 +1,31 @@
-enum Trafficlights { Green, Red, Yellow }; 
-static void Main(string[] args) 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+	class Program
+	{
+		enum Trafficlights { Green, Red, Yellow }; 
+        static void Main(string[] args) 
 { 
     TrafficLights x = TrafficLights.Red; 
-    switch (x) { case TrafficLights.Green; 
+    switch (x) { 
+    case TrafficLights.Green: 
     Console.Writeline("Go!"); 
     break; 
-    case Trafficlights.Red;
+    case Trafficlights.Red:
     Console.WriteLine("Stop!");
     break;
-    case Trafficlights.Yellow; 
+    case Trafficlights.Yellow: 
     Console.WriteLine("Caution!"); 
     break;
-   { 
-       {
+   
+    }
+    }
+    }
+    }      
+
