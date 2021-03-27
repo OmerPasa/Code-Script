1e3d9e05f539246b59ed8362f5ce47e869ee3aa0
OmerPasa3328 <omerfa33@gmail.com>
2021-03-27 15:27:48 +0300

    yay  I can understand expections

--- robot  barmen.cs
+++ robot  barmen.cs
@@ -1,4 +1,4 @@
-using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,18 +16,20 @@ namespace SoloLearn
                 int shelves = Convert.ToInt32(Console.ReadLine());
 
                 //your code goes here
+                //drinks / shelves = shelvesperdrink
 
             }
             /*
              * 1. DivideByZeroException => "At least 1 shelf"
              * 2. FormatException => "Please insert an integer"
             */
-            catch ()
+            catch (DivideByZeroException oex)
             {
-
+                Console.WriteLine(oex.Message);
             }
-            catch ()
+            catch (FormatException fex)
             {
+                Console.WriteLine(fex.Message);
 
             }
         }
