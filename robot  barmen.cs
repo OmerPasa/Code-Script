002a48fecd7320ac7dfd91f7140d33022f196421
OmerPasa3328 <omerfa33@gmail.com>
2021-03-27 11:51:47 +0300

    just starting

---
+++ robot  barmen.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int drinks = Convert.ToInt32(Console.ReadLine());
+                int shelves = Convert.ToInt32(Console.ReadLine());
+
+                //your code goes here
+
+            }
+            /*
+             * 1. DivideByZeroException => "At least 1 shelf"
+             * 2. FormatException => "Please insert an integer"
+            */
+            catch ()
+            {
+
+            }
+            catch ()
+            {
+
+            }
+        }
+    }
+}
